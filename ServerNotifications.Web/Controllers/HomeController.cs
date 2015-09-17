55e4ad0c47c32859d88dc7053fe23a03fa3bfd33
Agustin Camino <agustin.camino@gmail.com>
2015-09-17 16:32:48 -0500

    add logic to handle exceptions globally

--- ServerNotifications.Web/Controllers/HomeController.cs
+++ ServerNotifications.Web/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using ServerNotifications.Web.Models;
-using ServerNotifications.Web.Models.Repository;
 
 namespace ServerNotifications.Web.Controllers
 {
@@ -12,21 +7,9 @@ namespace ServerNotifications.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            // We reach some exceptional condition, and the
+            // applicaiton is gonna throw an exeption.
+            throw new Exception("Something went wrong");
         }
     }
 }
--- ServerNotifications.Web/Global.asax.cs
+++ ServerNotifications.Web/Global.asax.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ServerNotifications.Web.Domain;
+using ServerNotifications.Web.Domain.Twilio;
+using ServerNotifications.Web.Models.Repository;
 
 namespace ServerNotifications.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +17,14 @@ namespace ServerNotifications.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // At this point we should capture the last exception thrown by doing
+            // var exception = Server.GetLastError();
+            var notifier = new Notifier(new AdministratorsRepository(), new RestClient());
+            notifier.SendMessages();
         }
     }
 }
